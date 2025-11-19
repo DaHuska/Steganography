bfa649943c9a126a20fedf026a318a72d9c964da
DaHuska <aboyanov@abv.bg>
2025-11-19 23:42:38 +0200

    Init commit

---
+++ steganography/src/main/java/bg/tusofia/steganography/SteganographyApplication.java
@@ -0,0 +1,20 @@
+package bg.tusofia.steganography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+@SpringBootApplication
+public class SteganographyApplication {
+
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(SteganographyApplication.class, args);
+
+		EncryptLSB encryptor = new EncryptLSB();
+	}
+
+}
