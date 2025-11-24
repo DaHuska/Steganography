ac4fe197ef6c5d0992685a7c3496f1f263681abc
DaHuska <aboyanov@abv.bg>
2025-11-24 10:14:51 +0200

    Add: encode message into pixels

--- steganography/src/main/java/bg/tusofia/steganography/EncryptLSB.java
+++ steganography/src/main/java/bg/tusofia/steganography/EncryptLSB.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.WritableRaster;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EncryptLSB {
     // TODO: rename variable
@@ -14,18 +16,52 @@ public class EncryptLSB {
 
     public void encryptMessageInImage(BufferedImage image, String message) {
         // TODO: Validate message length and image pixels
+        BufferedImage imageCopy = getImageCopy(image);
+        Pixel[] imagePixels = getPixels(imageCopy);
+        String[] messageBinary = convertMessageToBinary(message);
+        String messageBinaryText = convertMessageBinaryArrtoString(messageBinary);
 
+        int index = 0;
+        for (int i = 0; i < imagePixels.length; i++) {
+            // Use same pixel object
+            // Check if message reaches end
+            Pixel currPixel = imagePixels[i];
+            if (index + 3 <= messageBinaryText.length()) {
+                encodeIntoPixel(currPixel, messageBinaryText.substring(index, index + 3));
+
+                index+=3;
+            } else {
+                encodeIntoPixel(currPixel, messageBinaryText.substring(index, messageBinaryText.length() - 1));
+            }
+
+            imagePixels[i] = currPixel;
+        }
     }
 
-    private BufferedImage getImageCopy(BufferedImage image) {
-        ColorModel colorModel = image.getColorModel();
-        boolean isAlphaPremultiplied = colorModel.isAlphaPremultiplied();
-        WritableRaster writableRaster = image.copyData(image.getRaster().createCompatibleWritableRaster());
+    private void encodeIntoPixel(Pixel pixel, String bits) {
+        Map<String, String> colorsBinary = convertPixelColorsToBinary(pixel);
 
-        return new BufferedImage(colorModel, writableRaster, isAlphaPremultiplied, null);
+        // TODO: continue with next LSBs if current LSBs run out
+        if (bits.length() == 1) {
+            colorsBinary.put("red", colorsBinary.get("red").substring(0, 7) + bits.charAt(0));
+        } else if (bits.length() == 2) {
+            colorsBinary.put("red", colorsBinary.get("red").substring(0, 7) + bits.charAt(0));
+            colorsBinary.put("green", colorsBinary.get("green").substring(0, 7) + bits.charAt(1));
+        } else if (bits.length() == 3) {
+            colorsBinary.put("red", colorsBinary.get("red").substring(0, 7) + bits.charAt(0));
+            colorsBinary.put("green", colorsBinary.get("green").substring(0, 7) + bits.charAt(1));
+            colorsBinary.put("blue", colorsBinary.get("blue").substring(0, 7) + bits.charAt(2));
+        }
+
+
+        Color newColor = new Color(
+                Integer.parseInt(colorsBinary.get("red"), 2),
+                Integer.parseInt(colorsBinary.get("green"), 2),
+                Integer.parseInt(colorsBinary.get("blue"), 2)
+        );
+
+        pixel.setColor(newColor);
     }
-
-
 
     private String[] convertMessageToBinary(String message) {
         String[] binaryValues = new String[message.length()];
@@ -38,6 +74,16 @@ public class EncryptLSB {
         }
 
         return binaryValues;
+    }
+
+    private String convertMessageBinaryArrtoString(String[] messageBinary) {
+        StringBuilder message = new StringBuilder();
+
+        for (String binary : messageBinary) {
+            message.append(binary);
+        }
+
+        return message.toString();
     }
 
     private Map<String, String> convertPixelColorsToBinary(Pixel pixel) {
